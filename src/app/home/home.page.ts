b54edc01e14ad71e50a9d0db4dff3dd0da7d00c1
milton2322 <miltonjoseq@gmail.com>
2022-03-10 00:25:08 -0500

    se agrega los cambios donde recorre el array que pinta los botones y la funcion para parsear los numeros

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,61 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  valor = '0';
+  valorA = '0';
+  operation = '';
 
+  newItem = true;
+  digitosGroup = [
+    [7, 8, 9, 'x'],
+    [4, 5, 6, '-'],
+    [1, 2, 3, '+'],
+    [0, 'C', '/', '=']
+  ];
+
+  onPress(symbol){
+    console.log(symbol);
+
+    if(this.numeric(symbol)){
+      console.log("es un numero");
+
+      if(this.newItem){
+        this.valor = '' + symbol;
+      }else{
+        this.valor += '' + symbol;
+        this.newItem = false;
+      }
+      
+    }
+    else if(symbol === 'C'){
+      this.valor = '0';
+      this.newItem = true;
+      
+    }
+
+    else if(symbol === '='){
+      if(this.operation === 'x'){//producto
+        this.valor = '' + (parseInt(this.valorA) * parseInt(this.valor));
+      }else if(this.operation === '-'){//resta
+        this.valor = '' + (parseInt(this.valorA) - parseInt(this.valor));
+      }else if(this.operation === '+'){//suma
+        this.valor = '' + (parseInt(this.valorA) + parseInt(this.valor));
+      }else if(this.operation === '/'){//dividir
+        this.valor = '' + (parseInt(this.valorA) / parseInt(this.valor));
+      this.newItem = true;
+      }
+    }
+    else{
+      this.newItem = true;
+      this.valorA = this.valor;
+      this.operation = symbol;
+    }
+    
+  }
   constructor() {}
+
+  numeric(n){
+    return !isNaN(parseFloat(n)) && !isNaN(n - 0);
+  }
 
 }
